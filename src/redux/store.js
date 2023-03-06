ebf12a408c33815f1f4204943b254fc9c9949e17
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-03-06 13:05:32 -0500

    using local and session storage to store initial state(login)

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,10 +10,23 @@ const reducer = combineReducers({
   userRegisterLogin: userRegisterLoginReducer,
 });
 
+const userInfoInLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : sessionStorage.getItem("userInfo")
+  ? JSON.parse(sessionStorage.getItem("userInfo"))
+  : {};
+
+const INITIAL_STATE = {
+  cart: {
+    value: 0,
+  },
+  userRegisterLogin: { userInfo: userInfoInLocalStorage },
+};
+
 const middleware = [thunk];
 const store = createStore(
   reducer,
-  { cart: { value: 0 } },
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
