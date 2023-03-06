d89cc74b1496bccf5daf13a3200f8070481ef12d
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-03-06 14:57:07 -0500

    control spinner while registering new user

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,5 +1,7 @@
 import RegisterPageComponent from "./components/RegisterPageComponent";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setReduxUserState } from "../redux/actions/userActions";
 
 const registerUserApiRequest = async (name, lastName, email, password) => {
   const { data } = await axios.post("/api/users/register", {
@@ -12,8 +14,13 @@ const registerUserApiRequest = async (name, lastName, email, password) => {
 };
 
 const RegisterPage = () => {
+  const reduxDispatch = useDispatch();
   return (
-    <RegisterPageComponent registerUserApiRequest={registerUserApiRequest} />
+    <RegisterPageComponent
+      registerUserApiRequest={registerUserApiRequest}
+      reduxDispatch={reduxDispatch}
+      setReduxUserState={setReduxUserState}
+    />
   );
 };
 
