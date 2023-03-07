58321efe20ec76fbc53ddae50e0ad0918e071728
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-03-07 12:47:00 -0500

    store cart items in local storage

--- src/redux/reducers/cartReducers.js
+++ src/redux/reducers/cartReducers.js
@@ -20,10 +20,31 @@ export const cartReducer = (state = CART_INITIAL_STATE, action) => {
       if (productAlreadyExistsInState) {
         currentState.itemsCount = 0;
         currentState.cartSubtotal = 0;
-        currentState.cartItems = productAlreadyExistsInState;
+        currentState.cartItems = state.cartItems.map((x) => {
+          // add product and sum quantity and price
+          if (x.productID === productAlreadyExistsInState.productID) {
+            currentState.itemsCount += Number(productBeingAddedToCart.quantity);
+            const sum =
+              Number(productBeingAddedToCart.quantity) *
+              Number(productBeingAddedToCart.price);
+            currentState.cartSubtotal += sum;
+          } else {
+            //if product doesn`t exists just upadte quantity and sum
+            currentState.itemsCount += Number(x.quantity);
+            const sum = Number(x.quantity) * Number(x.price);
+            currentState.cartSubtotal += sum;
+          }
+          return x.productID === productAlreadyExistsInState.productID
+            ? productBeingAddedToCart
+            : x;
+        });
       } else {
-        currentState.itemsCount = "x";
-        currentState.cartSubtotal = "x";
+        currentState.itemsCount += Number(productBeingAddedToCart.quantity);
+        const sum =
+          Number(productBeingAddedToCart.quantity) *
+          Number(productBeingAddedToCart.price);
+        currentState.cartSubtotal += sum;
+        currentState.cartItems = [...state.cartItems, productBeingAddedToCart];
       }
 
       return currentState;
