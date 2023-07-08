e100982f1f04871c1b1c5a28b3dee51eecec2fcc
unknown <jorge.a.arbelaez.c@gmail.com>
2023-07-08 08:32:24 -0500

    get product details

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -3,17 +3,22 @@ import { useEffect } from "react";
 
 import { useDispatch } from "react-redux";
 
+
 import { addToCart } from "../redux/actions/cartActions";
+import axios from 'axios'
+
+const getProductDetails = async(id) => {
+    const { data } = await axios.get(`/api/products/get-one/${id}`);
+    return data
+}
 
 const ProductDetailsPage = () => {
-  const dispatch = useDispatch();
 
-  return (
-    <ProductDetailsPageComponent
-      addToCartReduxAction={addToCart}
-      reduxDispatch={dispatch}
-    />
-  );
+    const dispatch = useDispatch()
+
+
+  return <ProductDetailsPageComponent  addToCartReduxAction={addToCart} reduxDispatch={dispatch} getProductDetails={getProductDetails}  />;
 };
 
 export default ProductDetailsPage;
+
