6b27c56f8858eb2c02300fc5b5b3f81e38a3cf52
unknown <jorge.a.arbelaez.c@gmail.com>
2023-07-18 20:26:01 -0500

    finish pagination

--- src/App.js
+++ src/App.js
@@ -43,13 +43,13 @@ function App() {
         <Route element={<RoutesWithUserChatComponent />}>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/product-list" element={<ProductListPage />}></Route>
-          <Route path="/product-list/:pageNum" element={<ProductListPage />}></Route>
+          <Route path="/product-list/:pageNumParam" element={<ProductListPage />}></Route>
           <Route path="/product-list/category/:categoryName" element={<ProductListPage />}></Route>
-          <Route path="/product-list/category/:categoryName/:pageNum" element={<ProductListPage />}></Route>
+          <Route path="/product-list/category/:categoryName/:pageNumParam" element={<ProductListPage />}></Route>
           <Route path="/product-list/search/:searchQuery" element={<ProductListPage />}></Route>
-          <Route path="/product-list/search/:searchQuery/:pageNum" element={<ProductListPage />}></Route>
+          <Route path="/product-list/search/:searchQuery/:pageNumParam" element={<ProductListPage />}></Route>
           <Route path="/product-list/category/:categoryName/search/:searchQuery" element={<ProductListPage />}></Route>
-          <Route path="/product-list/category/:categoryName/search/:searchQuery/:pageNum" element={<ProductListPage />}></Route>
+          <Route path="/product-list/category/:categoryName/search/:searchQuery/:pageNumParam" element={<ProductListPage />}></Route>
           <Route path="/product-details/:id" element={<ProductDetailsPage />}></Route>
           <Route path="/cart" element={<CartPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
